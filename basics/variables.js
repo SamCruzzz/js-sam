941a4bed0c13b5aff9a6b7646d0289a26cc2f43f
Soumya <102266530+SamCruzzz@users.noreply.github.com>
2024-07-23 19:08:06 +0530

    Update variables.js

--- basics/variables.js
+++ basics/variables.js
@@ -1,1 +1,30 @@
+"use strict" //treat all JS code as newer version //
 
+// alert(3+3) //we r using nodejs not browser //
+
+console.log(3 + 3)
+
+// code readability should be high
+
+console.log("Soumya")
+
+let name = "Soumya"
+let age = "24"
+let isLoggedIn = "True"
+let state;
+
+// Difference between null and undefined
+
+/*  let state = "null" ( standalone )
+let state ;  ( undefined - value not assigned yet) */
+
+//number => 2 to the power 53
+// bigint
+//string => ""
+//boolean => True/False
+//null => standalone value
+//undefined =>  not assigned yet 
+// symbol => unique
+  /*   null  is a object */
+
+console.log(typeof 24 );
